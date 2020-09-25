04d52e0d606664f91eb954f9f29b89b7479647b0
mshaw34 <58455048+mshaw34@users.noreply.github.com>
2020-09-25 14:41:40 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 input.buttonA.onEvent(ButtonEvent.Click, function () {
     makerController.player1.press(ArcadeButton.Up)
     music.pewPew.play()
-    pause(200)
+    pause(50)
 })
 forever(function () {
     makerController.player1.setAnalog(ArcadeAnalogButton.LeftRight, input.acceleration(Dimension.X))
